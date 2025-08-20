51dfd4ca8b28e75acc80a6ebf9e26b8412ffddfd
theTarnished <alex4750384@gmail.com>
2025-08-20 19:13:35 -0400

    feat: add the logic game

--- game_logic.py
+++ game_logic.py
@@ -22,18 +22,26 @@ def get_angle_radians(point_a: Point2D, point_b: Point2D) -> float:
     ### ---------------------- ###
     ### SU IMPLEMENTACION AQUI ###
     ### ---------------------- ###
-    return 0.0
+    dx = point_b.x - point_a.x
+    dy = point_b.y - point_a.y
+    return math.atan2(dy, dx)
 
 
 def get_distance(point_a: Point2D, point_b: Point2D) -> float:
     ### ---------------------- ###
     ### SU IMPLEMENTACION AQUI ###
     ### ---------------------- ###
-    return 0.0
+    dx = point_b.x - point_a.x
+    dy = point_b.y - point_a.y
+    return math.sqrt(dx * dx + dy * dy)
 
 
 def get_impulse_vector(start_point: Point2D, end_point: Point2D) -> ImpulseVector:
     ### ---------------------- ###
     ### SU IMPLEMENTACION AQUI ###
     ### ---------------------- ###
-    return ImpulseVector(0, 0)
+    angle = get_angle_radians(start_point, end_point)
+    # Reverse the direction by adding π (180 degrees) for slingshot effect
+    opposite_angle = angle + math.pi
+    impulse = get_distance(start_point, end_point)
+    return ImpulseVector(opposite_angle, impulse)
